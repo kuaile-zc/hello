c3b4514e1ecfb552671739ce9e83ab72b4e60ee6
kuaile-zc <784554215@qq.com>
2019-03-17 12:55:51 +0800

    add stream demo

---
+++ src/java/com/zc/Lambdas/stream/FlatMapAndMapCompara.java
@@ -0,0 +1,57 @@
+package com.zc.Lambdas.stream;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 描述:
+ *
+ * @author zc
+ * @create 2019-03-17 12:15
+ */
+public class FlatMapAndMapCompara {
+
+    public static void main(String[] args) {
+            Demo();
+    }
+
+
+    /**
+     * Use (1,2,3) and (3,4)
+     * Joint to the
+     * {a.1,b.1},{a.1,b,2}
+     * Example
+     *
+     * {1,3},{1,4}
+     * {2,3},{2,4}
+     * {3,3},{3,4}
+     */
+    private static void Demo(){
+        List<Integer> number1 = Arrays.asList(1,2,3);
+        List<Integer> number2 = Arrays.asList(3,4);
+        List<int[]> pairs= number1.stream()
+                .flatMap(i -> number2.stream().map(j -> new int[]{i,j}))
+                .collect(Collectors.toList());
+        pairs.stream().forEach(x->
+        {
+            System.out.print("{");
+            final int[] num = {1};
+            Arrays.stream(x)
+                    .forEach(y->
+                        {
+                            System.out.print(y) ;
+                            if (num[0] ==1){
+                                System.out.print(",");
+                                num[0] -=1;
+                            }
+                        }
+            );
+            System.out.println("}");
+        } );
+
+
+    }
+}
---
+++ src/java/com/zc/Lambdas/stream/Main.java
@@ -0,0 +1,20 @@
+package com.zc.Lambdas.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 描述:
+ *
+ * @author zc
+ * @create 2019-03-14 21:00
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
