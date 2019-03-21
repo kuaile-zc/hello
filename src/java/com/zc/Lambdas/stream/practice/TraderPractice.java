2bf6d10451bbc0992530d85997078d120d815cdb
zhangchen <784554215@qq.com>
2019-03-21 17:15:07 +0800

    add samlpe

--- src/java/com/zc/Lambdas/stream/practice/TraderPractice.java
+++ src/java/com/zc/Lambdas/stream/practice/TraderPractice.java
@@ -2,6 +2,7 @@ package com.zc.Lambdas.stream.practice;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -12,8 +13,15 @@ import java.util.stream.Collectors;
  */
 public class TraderPractice {
 
+    private static final List<Transction> allTransctionList = TraderUtil.getTransaction();
+
     public static void main(String[] args) {
         get_2011_Trader();
+        get_all_city();
+        get_cambridge_sort_name();
+        get_all_name();
+        get_max_value();
+        get_min_transction();
     }
 
     /**
@@ -21,11 +29,67 @@ public class TraderPractice {
      *
      */
     private static void get_2011_Trader(){
-        List<Transction> transctionList = TraderUtil.getTransaction().stream()
+        List<Transction> transctionList = allTransctionList.stream()
                 .filter(x->x.getYear()==2011)
                 .sorted(Comparator.comparing(Transction::getValue) )
                 .collect(Collectors.toList());
 
         System.out.println(transctionList);
     }
+
+
+    /**
+     * Find all city value .
+     */
+    private static void get_all_city(){
+        List<String> all_city = allTransctionList.stream()
+                .map(Transction::getTrader).map(Trader::getCity)
+                .distinct().collect(Collectors.toList());
+        System.out.println(all_city);
+
+    }
+
+    /**
+     * Find trader city is Cambridge list, sorted by name.
+     */
+    private static void get_cambridge_sort_name(){
+        List<Trader> tradersList  = allTransctionList.stream()
+                .map(Transction::getTrader)
+                .filter(x->x.getCity()=="Cambridge").distinct()
+                .sorted(Comparator.comparing(Trader::getName))
+                .collect(Collectors.toList());
+        System.out.println(tradersList);
+    }
+
+    /**
+     * Find all trader's name.
+     */
+    private static void get_all_name(){
+        List<String> nameList = allTransctionList.stream()
+                .map(Transction::getTrader).map(Trader::getName)
+                .distinct()
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.toList());
+        System.out.println(nameList);
+    }
+
+    /**
+     * Mix value
+     */
+    private static void get_max_value(){
+        Optional<Integer> maxValue = allTransctionList.stream().map(Transction::getValue)
+                .reduce(Integer::max);
+        System.out.println(maxValue.get());
+    }
+
+    /**
+     * Min transction
+     */
+    private static void get_min_transction(){
+        Optional<Transction> minTransction  = allTransctionList.stream().min(Comparator.comparing(Transction::getValue));
+        System.out.println(minTransction.get());
+
+    }
+
+
 }
