73659a4cd525301400979afecfc3e8a5eac6d645
kuaile-zc <784554215@qq.com>
2019-04-14 23:01:05 +0800

    add collect

--- src/java/com/zc/Lambdas/stream/Main.java
+++ src/java/com/zc/Lambdas/stream/Main.java
@@ -2,7 +2,10 @@ package com.zc.Lambdas.stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 描述:
@@ -14,6 +17,9 @@ public class Main {
     public static void main(String[] args) {
         FlatMapAndMapCompara.Demo();
         ReduceDemo.mapAndReduce();
+        List<Dish> lists = MenuUtil.getMenu();
+        String name = lists.stream().map(Dish::getName).collect(Collectors.joining(","));
+        System.out.println(name);
     }
 
 
