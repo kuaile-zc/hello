9c4280f9c2319dd9b941050d95cef194c2431e13
zhangchen <784554215@qq.com>
2021-01-01 18:24:00 +0800

    Daily code.

---
+++ src/main/java/com/zc/designMode/reference/Key.java
@@ -0,0 +1,12 @@
+package com.zc.designMode.reference;
+
+/**
+ * @author CoreyChen Zhang
+ * @version 2020/12/31 16:01
+ * @modified
+ */
+public class Key extends Element{
+    public Key(String id) {
+        super(id);
+    }
+}
--- src/main/java/com/zc/designMode/reference/Main.java
+++ src/main/java/com/zc/designMode/reference/Main.java
@@ -1,6 +1,9 @@
 package com.zc.designMode.reference;
 
 /**
+ *
+ * 文档：java中的四种引用类型.note
+ * 链接：http://note.youdao.com/noteshare?id=f4da15b2e9736575c5a107fe394cdc55&sub=A53DEA71AD4C4111A7857F0927C7C771
  * 通过软应用实现高速缓存
  *
  * @author CoreyChen Zhang
@@ -12,5 +15,6 @@ public class Main {
         EmployeeCache employeeCache = EmployeeCache.getInstance();
         Employee employee = employeeCache.getEmployee("123");
         System.out.println(employee);
+
     }
 }
