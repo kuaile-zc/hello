f263e36684a6ad53be7a380b2c32553098a17d98
zhangchen <784554215@qq.com>
2021-02-19 10:26:52 +0800

    Test new version git.

--- src/main/java/com/zc/leetcode/MaxConsecutiveOne3.java
+++ src/main/java/com/zc/leetcode/MaxConsecutiveOne3.java
@@ -35,7 +35,7 @@ package com.zc.leetcode;
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/max-consecutive-ones-iii
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。    
  * @author CoreyChen Zhang
  * @version 2021/2/19 9:38
  * @modified
