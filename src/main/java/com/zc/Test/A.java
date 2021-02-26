7e1fd54d4406e476d78041d13f9a8b9120253001
zhangchen <784554215@qq.com>
2021-02-26 17:13:27 +0800

    Daily code.

---
+++ src/main/java/com/zc/Test/A.java
@@ -0,0 +1,13 @@
+package com.zc.Test;
+
+/**
+ * @author CoreyChen Zhang
+ * @version 2021/2/25 17:06
+ * @modified
+ */
+public class A {
+
+    public static void main(String[] args) {
+        Object o = new Object();
+    }
+}
