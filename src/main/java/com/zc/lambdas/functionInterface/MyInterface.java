ef8619283b27df592de1b9892fda6464b28df26a
Corey <784554215@qq.com>
2022-03-12 19:39:25 +0800

    Add function annotation demo！ Maybe we need to know more java-lambda demo!

---
+++ src/main/java/com/zc/lambdas/functionInterface/MyInterface.java
@@ -0,0 +1,12 @@
+package com.zc.lambdas.functionInterface;
+
+/**
+ * @author CoreyChen Zhang
+ * @date 2022/3/12 0:23
+ * @modified
+ */
+@FunctionalInterface
+public interface MyInterface {
+
+    void FunctionPrint (String msg);
+}
